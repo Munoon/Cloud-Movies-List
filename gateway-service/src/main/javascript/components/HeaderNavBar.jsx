e8d19a0d92cda3cf130bf5668a12c982cb24f023
Munoon <Munoongg@gmail.com>
2020-07-15 13:57:30 +0300

    Refactor design with react bootstrap

--- gateway-service/src/main/javascript/components/HeaderNavBar.jsx
+++ gateway-service/src/main/javascript/components/HeaderNavBar.jsx
@@ -1,23 +1,19 @@
 import React from 'react';
-import 'bootstrap/js/dist/dropdown';
 import { getMetaProperty }  from './misc';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 const HeaderNavBar = ({ userAuthenticated }) => (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-            <a className="navbar-brand" href="/">Movies List</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"/>
-            </button>
-            <div className="collapse navbar-collapse d-flex">
-                <ul className="navbar-nav mr-auto mb-2 mb-lg-0">
-                    <UserNavBarItem {...{ userAuthenticated }} />
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <Navbar bg="light" expand="lg" style={{ padding: '15px' }}>
+        <Navbar.Brand href="/">Movies List</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse>
+            <Nav className="mr-auto">
+                <UserNavBarItem userAuthenticated={userAuthenticated} />
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
 );
 
 const UserNavBarItem = ({ userAuthenticated }) => userAuthenticated
@@ -26,27 +22,16 @@ const UserNavBarItem = ({ userAuthenticated }) => userAuthenticated
 
 const ProfileUserNavBarItem = () => {
     return (
-        <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="#" id="userNavbarDropdown" role="button"
-               data-toggle="dropdown" aria-expanded="false">
-                {getMetaProperty('user:name')} {getMetaProperty('user:surname')}
-            </a>
-            <ul className="dropdown-menu" aria-labelledby="userNavbarDropdown">
-                <li>
-                    <a className="dropdown-item disabled" href="#" aria-disabled="true">{getMetaProperty('user:email')}</a>
-                </li>
-                <li>
-                    <a className="dropdown-item" href="/logout">Выйти</a>
-                </li>
-            </ul>
-        </li>
+        <NavDropdown title={getMetaProperty('user:name') + ' ' + getMetaProperty('user:surname')} id="userNavbarDropdown">
+            <NavDropdown.Item className='disabled'>{getMetaProperty('user:email')}</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="/logout">Выйти</NavDropdown.Item>
+        </NavDropdown>
     );
 }
 
 const LoginUserNavBarItem = () => (
-    <li className="nav-item">
-        <a className="nav-link active" aria-current="page" href="/login">Войти</a>
-    </li>
+    <Nav.Link href="/login">Войти</Nav.Link>
 );
 
 export default HeaderNavBar;
