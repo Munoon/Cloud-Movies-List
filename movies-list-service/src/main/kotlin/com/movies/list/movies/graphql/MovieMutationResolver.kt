050bf84972ed16152150df339692e2e8dd3fe918
Munoon <munoongg@gmail.com>
2020-10-29 00:35:29 +0200

    Fix tests in movies-list-service

--- movies-list-service/src/main/kotlin/com/movies/list/movies/graphql/MovieMutationResolver.kt
+++ movies-list-service/src/main/kotlin/com/movies/list/movies/graphql/MovieMutationResolver.kt
@@ -4,6 +4,7 @@ import com.movies.list.movies.MovieMapper
 import com.movies.list.movies.MoviesService
 import com.movies.list.movies.to.CreateMoviesTo
 import com.movies.list.movies.to.MovieTo
+import com.movies.list.utils.SecurityUtils.authUserId
 import graphql.kickstart.tools.GraphQLMutationResolver
 import org.slf4j.LoggerFactory
 import org.springframework.security.access.prepost.PreAuthorize
@@ -18,7 +19,7 @@ class MovieMutationResolver(private val moviesService: MoviesService): GraphQLMu
 
     @PreAuthorize("hasRole('ADMIN')")
     fun addMovie(@Valid createMoviesTo: CreateMoviesTo): MovieTo {
-        log.info("Add movie $createMoviesTo")
+        log.info("Add movie $createMoviesTo by user ${authUserId()}")
         val movie = moviesService.createMovie(createMoviesTo);
         return MovieMapper.INSTANCE.asMovieTo(movie)
     }
--- movies-list-service/src/test/kotlin/com/movies/list/AbstractTest.kt
+++ movies-list-service/src/test/kotlin/com/movies/list/AbstractTest.kt
@@ -8,7 +8,9 @@ import com.movies.list.utils.GraphQLRestTemplate
 import com.movies.list.utils.TestUtils
 import org.junit.jupiter.api.BeforeEach
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
@@ -21,13 +23,15 @@ import org.springframework.security.oauth2.provider.OAuth2Request
 import org.springframework.security.oauth2.provider.token.TokenStore
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter
 import java.util.*
+import org.mockito.Mockito.`when` as mockWhen
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 abstract class AbstractTest {
     @Autowired
     private lateinit var mongoTemplate: MongoTemplate
 
-    @Autowired
+    @MockBean
+    @Qualifier("jwtTokenStore")
     private lateinit var tokenStore: TokenStore
 
     @BeforeEach
@@ -41,7 +45,8 @@ abstract class AbstractTest {
         val authentication = OAuth2Authentication(oAuth2Request, token)
 
         val accessToken = DefaultOAuth2AccessToken(UUID.randomUUID().toString())
-        tokenStore.storeAccessToken(accessToken, authentication)
+        mockWhen(tokenStore.readAccessToken(accessToken.value)).thenReturn(accessToken)
+        mockWhen(tokenStore.readAuthentication(accessToken)).thenReturn(authentication)
         return accessToken
     }
 
